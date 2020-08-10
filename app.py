4fcb9523e32109e367e47148465740ced859dce8
misza80 <55979600+misza80@users.noreply.github.com>
2020-08-10 18:46:44 +0100

    added recipes page + route connected mongodb

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
+
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'recipeDB'
-app.config["MONGO_URI"] = 'mongodb+srv://db_admin:<password>@cluster0.18nsj.azure.mongodb.net/<dbname>?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://db_admin:klop9000@cluster0.18nsj.azure.mongodb.net/recipeDB?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
+# Route to home page
 @app.route('/')
+@app.route('/home/')
 def home():
     return render_template('home.html')
 
+#Route to recipes page
+@app.route('/recipes/')
+def get_recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run()
