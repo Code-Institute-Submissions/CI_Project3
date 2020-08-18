f8a3399b86bc9dfe5ff0ee1e60669844b8eb4d89
misza80 <55979600+misza80@users.noreply.github.com>
2020-08-18 14:09:36 +0100

    added function for card size change depending on viewport

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
     $('.carousel').carousel();
@@ -6,3 +6,14 @@ $(document).ready(function() {
 });
 
 
+$(window).resize(function () {
+    if ($(window).width() <= 1024) {
+        $('#singlecard').removeClass('large');
+    }
+    else if ($(window).width() <= 767){
+        $('#singlecard').removeClass('horizontal large');
+    }
+    else {
+        $('#singlecard').addClass('horizontal large');
+    }
+});
