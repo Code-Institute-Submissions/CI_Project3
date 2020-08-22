ea8cf08ada1a3b745788f3311a94e33d45d683b3
misza80 <55979600+misza80@users.noreply.github.com>
2020-08-22 14:21:53 +0100

    added form validation and maximum length

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,17 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
     $('.carousel').carousel();
-    $('select').formSelect();
+
     $('.modal').modal();
+    $('input#input_text, textarea#textarea2').characterCounter();
+    $('select').formSelect();
+    // for HTML5 "required" attribute
+    $("select[required]").css({
+        display: "inline",
+        height: 0,
+        padding: 0,
+        width: 0
+    });
 });
 
 
