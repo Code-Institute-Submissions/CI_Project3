8ebda21132e234de297ba3dc5feb87c91fe8b187
misza80 <55979600+misza80@users.noreply.github.com>
2020-08-22 14:41:17 +0100

    Delete env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Create a secret key
-os.environ["SECRET_KEY"] = "Dyzio"
-# Storing the MONGO_URI environment variable
-os.environ[
-    "MONGO_URI"] = 'mongodb+srv://db_admin:kakao9000@cluster0.18nsj.azure.mongodb.net/recipeDB?retryWrites=true&w=majority'
-os.environ["MONGO_DBNAME"] = 'recipeDB'
